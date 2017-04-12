bc42442134bc8adef205af6b0098299b49ca35a6
Bojan Dimovski <bojand@gmail.com>
2017-04-12 22:39:51 +0200

    Add empty comparison tests.

---
+++ Unit Tests/ComparisonTests.swift
@@ -0,0 +1,25 @@
+//
+//  ComparisonTests.swift
+//  Thingy
+//
+//  Created by Bojan Dimovski on 07.4.17.
+//  Copyright (c) 2017 Bojan Dimovski. All rights reserved.
+//
+
+
+import XCTest
+@testable import Thingy
+
+class ComparisonTests: XCTestCase {
+
+	func testCompareDeviceIsEqual() {
+	}
+
+	func testCompareDeviceIsNewer() {
+
+	}
+
+	func testCompareDeviceIsOlder() {
+
+	}
+}
