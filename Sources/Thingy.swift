8fca9ef738f490407a1cb490db7845ab4cbe8b0b
Bojan Dimovski <bojand@gmail.com>
2017-04-12 22:37:58 +0200

    Merge the model into the main type.

--- Sources/Thingy.swift
+++ Sources/Thingy.swift
@@ -1,5 +1,5 @@
 //
-// Thingy.swift
+// Thingy
 // Thingy
 //
 // Created by Bojan Dimovski on 21.11.16.
@@ -26,103 +26,392 @@
 
 import Foundation
 
-/// Thingy is the main type used to inspect and identify your current or custom device.
+/// A type that describes the specific device model.
 ///
-/// It exposes properties such as the device family, model or marketing name.
-///
-/// Besides the device properties it provides a light-weight, yet powerful device comparison.
+/// For iOS devices, only devices supporting iOS 8 are included.
 
-public struct Thingy {
+public enum Thingy {
 
-	/// Specific device model.
-	public var model: Model?
+	/// - iPhone4S: iPhone 4S.
+	case iPhone4S
+	/// - iPhone5: All iPhone 5 variants.
+	case iPhone5
+	/// - iPhone5s: All iPhone 5s variants.
+	case iPhone5s
+	/// - iPhone5c: All iPhone 5c variants.
+	case iPhone5c
+	/// - iPhone6: All iPhone 6 variants.
+	case iPhone6
+	/// - iPhone6Plus: All iPhone 6 Plus variants.
+	case iPhone6Plus
+	/// - iPhone6s: All iPhone 6s variants.
+	case iPhone6s
+	/// - iPhone6sPlus: All iPhone 6s Plus variants.
+	case iPhone6sPlus
+	/// - iPhoneSE: iPhone SE.
+	case iPhoneSE
+	/// - iPhone7: All iPhone 7 variants.
+	case iPhone7
+	/// - iPhone7Plus: All iPhone 7 Plus variants.
+	case iPhone7Plus
 
-	/// General device family.
-	public var family: Family?
 
-	/// Specific product line.
-	public var productLine: ProductLine?
+	/// - iPad2: All iPad 2 variants.
+	case iPad2
+	/// - iPad3: All iPad 3 variants.
+	case iPad3
+	/// - iPad4: All iPad 4 variants.
+	case iPad4
+	/// - iPad5: All iPad 5 (2017) variants.
+	case iPad5
+	/// - iPadMini: The original iPad mini in all variants.
+	case iPadMini
+	/// - iPadMini2: All iPad mini 2 variants.
+	case iPadMini2
+	/// - iPadMini3: All iPad mini 3 variants.
+	case iPadMini3
+	/// - iPadMini4: All iPad mini 4 variants.
+	case iPadMini4
+	/// - iPadAir: The original iPad Air in all variants.
+	case iPadAir
+	/// - iPadAir2: All iPad Air 2 variants.
+	case iPadAir2
+	/// - iPadPro12Inch: The 12.9 inch iPad Pro in all variants.
+	case iPadPro12Inch
+	/// - iPadPro9Inch: The 9.7 inch iPad Pro in all variants.
+	case iPadPro9Inch
 
-	fileprivate var _identifier: String?
-	fileprivate var _rawValue: RawThingy?
+	/// - iPodTouch5G: All iPod touch 5G variants.
+	case iPodTouch5G
+	/// - iPodTouch6G: All iPod touch 6G variants.
+	case iPodTouch6G
+
+	/// - appleTV4: Apple TV 4.
+	case appleTV4
+
+	/// - watch: The original Apple Watch.
+	case watch
+	/// - watchSeries1: Apple Watch Series 1.
+	case watchSeries1
+	/// - watchSeries2: Apple Watch Series 2.
+	case watchSeries2
+
+	/// - simulator: A simulator for the associated device model.
+	indirect case simulator(Thingy)
+
+	/// - unknown: An unknown or a future device within the associated family.
+	case unknown(Family)
+
+	/// All real-device values.
+	internal static var allValues: [Thingy] = [
+			.iPhone4S,
+			.iPhone5,
+			.iPhone5c,
+			.iPhone5s,
+			.iPhone6,
+			.iPhone6Plus,
+			.iPhone6s,
+			.iPhone6sPlus,
+			.iPhoneSE,
+			.iPhone7,
+			.iPhone7Plus,
+			.iPodTouch5G,
+			.iPodTouch6G,
+			.appleTV4,
+			.watch,
+			.watchSeries1,
+			.watchSeries2,
+			.iPad2,
+			.iPad3,
+			.iPad4,
+			.iPad5,
+			.iPadAir,
+			.iPadAir2,
+			.iPadPro12Inch,
+			.iPadPro9Inch,
+			.iPadMini,
+			.iPadMini2,
+			.iPadMini3,
+			.iPadMini4,
+	]
+
+}
+
+extension Thingy {
 
 	/// Inspects your current device if the identifier is omitted, or creates a custom device out of a valid identifier.
 	///
+	/// If a device cannot be created, returns an unknown device.
+	///
 	/// - Parameter identifier: A device identifier, e.g. "iPhone9,2", "iPad6,11.", "AppleTV5,3".
-	public init(identifier: String? = nil) {
-		let identifier = identifier ?? self.identifier()
-
-		let rawThingy = RawThingy(identifier: identifier)
-		if let thingy = rawThingy.thingy {
-			self = thingy
-			_rawValue = rawThingy
+	public init?(identifier: String? = nil) {
+		guard let thingy = RawThingy(identifier: identifier).thingy
+		else {
+			return nil
 		}
+
+		self = thingy
 	}
 
 }
 
-// MARK: - Device identification
+// MARK: - Family
 
-internal extension Thingy {
+extension Thingy {
 
-	/// Extracts the device identifier from low-level APIs.
-	///
-	/// - Returns: A device identifier, e.g. "iPhone9,2", "iPad6,11", "AppleTV5,3".
-	internal mutating func identifier() -> String {
-		guard _identifier == nil
-		else {
-			return _identifier!
+	/// Associated family for each device.
+	var family: Family {
+		switch self {
+		case .iPhone4S,
+			 .iPhone5,
+			 .iPhone5c,
+			 .iPhone5s,
+			 .iPhone6,
+			 .iPhone6Plus,
+			 .iPhone6s,
+			 .iPhone6sPlus,
+			 .iPhoneSE,
+			 .iPhone7,
+			 .iPhone7Plus:
+			return .phone
+
+		case .iPodTouch5G,
+			 .iPodTouch6G:
+			return .pod
+
+		case .appleTV4:
+			return .tv
+
+		case .watch,
+			 .watchSeries1,
+			 .watchSeries2:
+			return .watch
+
+		case .iPad2,
+			 .iPad3,
+			 .iPad4,
+			 .iPad5,
+			 .iPadAir,
+			 .iPadAir2,
+			 .iPadPro12Inch,
+			 .iPadPro9Inch,
+			 .iPadMini,
+			 .iPadMini2,
+			 .iPadMini3,
+			 .iPadMini4:
+			return .pad
+
+		case let .simulator(model):
+			return model.family
+
+		case let .unknown(family):
+			return family
 		}
+	}
+}
 
-		var systemInfo = utsname()
-		uname(&systemInfo)
-		let machineMirror = Mirror(reflecting: systemInfo.machine)
-		let identifier = machineMirror.children.reduce("") { identifier, element in
-			guard let value = element.value as? Int8,
-				  value != 0
-			else {
-				return identifier
-			}
-			return identifier + String(UnicodeScalar(UInt8(value)))
-		}
+// MARK: - Model numbers
 
-		_identifier = identifier
-		return identifier
+extension Thingy {
+
+	internal var lowestNumber: Double {
+		return numbers.first ?? 0
 	}
 
-	/// Initializes a parsed device using all the properties.
-	///
-	/// - Parameters:
-	///   - family: A device family.
-	///   - model: A device model.
-	///   - productLine: A product line.
-	init(family: Family?, model: Model?, productLine: ProductLine?) {
-		self.family = family
-		self.model = model
-		self.productLine = productLine
-		self._identifier = identifier()
+	/// Associated model numbers for each device.
+	var numbers: [Double] {
+		switch self {
+		case .iPhone4S:
+			return [4.1]
+		case .iPhone5:
+			return [5.1, 5.2]
+		case .iPhone5c:
+			return [5.3, 5.4]
+		case .iPhone5s:
+			return [6.1, 6.2]
+		case .iPhone6:
+			return [7.2]
+		case .iPhone6Plus:
+			return [7.1]
+		case .iPhone6s:
+			return [8.1]
+		case .iPhone6sPlus:
+			return [8.2]
+		case .iPhoneSE:
+			return [8.4]
+		case .iPhone7:
+			return [9.1, 9.3]
+		case .iPhone7Plus:
+			return [9.2, 9.4]
+
+		case .iPodTouch5G:
+			return [5.1]
+		case .iPodTouch6G:
+			return [7.1]
+
+		case .appleTV4:
+			return [5.3]
+
+		case .watch:
+			return [1.1, 1.2]
+		case .watchSeries1:
+			return [2.6, 2.7]
+		case .watchSeries2:
+			return [2.3, 2.4]
+
+		case .iPad2:
+			return [2.1, 2.2, 2.3, 2.4]
+		case .iPad3:
+			return [3.1, 3.2, 3.3]
+		case .iPad4:
+			return [3.4, 3.5, 3.6]
+		case .iPad5:
+			return [6.11, 6.12]
+		case .iPadAir:
+			return [4.1, 4.2, 4.3]
+		case .iPadAir2:
+			return [5.3, 5.4]
+		case .iPadPro12Inch:
+			return [6.7, 6.8]
+		case .iPadPro9Inch:
+			return [6.3, 6.4]
+		case .iPadMini:
+			return [2.5, 2.6, 2.7]
+		case .iPadMini2:
+			return [4.4, 4.5, 4.6]
+		case .iPadMini3:
+			return [4.7, 4.8, 4.9]
+		case .iPadMini4:
+			return [5.1, 5.2]
+
+		case let .simulator(model):
+			return model.numbers
+
+		case .unknown(_):
+			return [0.0]
+		}
 	}
+}
+
+// MARK: - Product lines
+
+extension Thingy {
+
+	/// Product line of the model, currently supported only for the iPad.
+	var productLine: ProductLine? {
+		switch self {
+		case .iPad2:
+			return Lines.iPad.regular
+		case .iPad3:
+			return Lines.iPad.regular
+		case .iPad4:
+			return Lines.iPad.regular
+		case .iPad5:
+			return Lines.iPad.regular
+		case .iPadAir:
+			return Lines.iPad.air
+		case .iPadAir2:
+			return Lines.iPad.air
+		case .iPadPro12Inch:
+			return Lines.iPad.pro
+		case .iPadPro9Inch:
+			return Lines.iPad.pro
+		case .iPadMini:
+			return Lines.iPad.mini
+		case .iPadMini2:
+			return Lines.iPad.mini
+		case .iPadMini3:
+			return Lines.iPad.mini
+		case .iPadMini4:
+			return Lines.iPad.mini
+		default:
+			return nil
+		}
+	}
+
 }
 
 // MARK: - Marketing protocol
 
 extension Thingy: MarketingProtocol {
 
-	/// The marketing name of the device.
-	///
-	/// In case of future or undetectable devices the device is marked as e.g. "Unknown iPhone" or "Unknown Device".
+	/// The full marketing name of the model, e.g. "iPhone 7 Plus", "Apple TV 4".
 	public var marketingName: String {
-		guard
-				let model = model
-		else {
-			if let family = family?.marketingName {
-				return NSLocalizedString("Unknown \(family)", comment: "Unknown \(family)")
-			}
+		switch self {
+		case .iPhone4S:
+			return "iPhone 4S"
+		case .iPhone5:
+			return "iPhone 5"
+		case .iPhone5s:
+			return "iPhone 5s"
+		case .iPhone5c:
+			return "iPhone 5c"
+		case .iPhone6:
+			return "iPhone 6"
+		case .iPhone6Plus:
+			return "iPhone 6 Plus"
+		case .iPhone6s:
+			return "iPhone 6s"
+		case .iPhone6sPlus:
+			return "iPhone 6s Plus"
+		case .iPhoneSE:
+			return "iPhone SE"
+		case .iPhone7:
+			return "iPhone 7"
+		case .iPhone7Plus:
+			return "iPhone 7 Plus"
 
-			return NSLocalizedString("Unknown Device", comment: "Unknown Device")
+		case .iPad2:
+			return "iPad 2"
+		case .iPad3:
+			return "iPad 3"
+		case .iPad4:
+			return "iPad 4"
+		case .iPad5:
+			return "iPad 5"
+		case .iPadMini:
+			return "iPad mini"
+		case .iPadMini2:
+			return "iPad mini 2"
+		case .iPadMini3:
+			return "iPad mini 3"
+		case .iPadMini4:
+			return "iPad mini 4"
+		case .iPadAir:
+			return "iPad Air"
+		case .iPadAir2:
+			return "iPad Air 2"
+		case .iPadPro12Inch:
+			return "iPad Pro 12.9in"
+		case .iPadPro9Inch:
+			return "iPad Pro 9.7in"
+
+		case .iPodTouch5G:
+			return "iPod touch 5G"
+		case .iPodTouch6G:
+			return "iPod touch 6G"
+
+		case .appleTV4:
+			return "Apple TV 4"
+
+		case .watch:
+			return "Apple Watch"
+		case .watchSeries1:
+			return "Apple Watch Series 1"
+		case .watchSeries2:
+			return "Apple Watch Series 2"
+
+		case let .simulator(model):
+			return NSLocalizedString("Simulator (\(model.marketingName))", comment: "Simulator (\(model.marketingName))")
+
+		case let .unknown(family):
+			return NSLocalizedString("Unknown \(family.marketingName)", comment: "Unknown \(family.marketingName)")
 		}
 
-		return model.marketingName
+	}
+
+}
+
 	}
 
 }
