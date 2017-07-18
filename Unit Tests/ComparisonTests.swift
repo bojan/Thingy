5522032425b7645988728490f21eca222a60eea6
Bojan Dimovski <bojand@gmail.com>
2017-07-18 21:18:20 +0200

    Update the comparison tests.

--- Unit Tests/ComparisonTests.swift
+++ Unit Tests/ComparisonTests.swift
@@ -13,13 +13,67 @@ import XCTest
 class ComparisonTests: XCTestCase {
 
 	func testCompareDeviceIsEqual() {
+		let device = Thingy(identifier: "iPad4,9")
+		let comparisonModel = Thingy.iPadMini3
+
+		XCTAssertNotNil(device)
+		if let device = device {
+			do {
+				let result = try device.isEqual(to: comparisonModel)
+				XCTAssert(result)
+			}
+			catch {
+				XCTFail("The devices are incompatible.")
+			}
+		}
 	}
 
 	func testCompareDeviceIsNewer() {
+		let device = Thingy.iPadMini4
+		let comparisonModel = Thingy.iPadMini3
 
+		do {
+			let result = try device.isNewerOrEqual(than: comparisonModel)
+			XCTAssert(result)
+		}
+		catch {
+			XCTFail("The devices are incompatible.")
+		}
 	}
 
 	func testCompareDeviceIsOlder() {
+		let device = Thingy.iPadPro12Inch
+		let comparisonModel = Thingy.iPadPro10Inch
 
+		do {
+			let result = try device.isOlder(than: comparisonModel)
+			XCTAssert(result)
+		}
+		catch {
+			XCTFail("The devices are incompatible.")
+		}
 	}
+
+	func testCompareIncompatibleDevices() {
+		var lhs = Thingy.unknown(.pad)
+		var rhs = Thingy.iPadPro10Inch
+
+		XCTAssertThrows(expression: try lhs.isEqual(to: rhs))
+
+		lhs = Thingy.iPadPro12Inch2G
+		rhs = Thingy.unknown(.pad)
+
+		XCTAssertThrows(expression: try lhs.isEqual(to: rhs))
+
+		lhs = Thingy.iPadPro12Inch2G
+		rhs = Thingy.iPad5
+
+		XCTAssertThrows(expression: try lhs.isEqual(to: rhs))
+
+		lhs = Thingy.iPhone7
+		rhs = Thingy.appleTV4
+
+		XCTAssertThrows(expression: try lhs.isEqual(to: rhs))
+	}
+
 }
