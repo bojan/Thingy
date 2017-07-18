127b7e621c98749b8b60d15918ffdbe55076081e
Bojan Dimovski <bojand@gmail.com>
2017-07-18 22:23:17 +0200

    Add a display struct.

---
+++ Sources/Display.swift
@@ -0,0 +1,19 @@
+//
+//  Display.swift
+//  Thingy
+//
+//  Created by Bojan Dimovski on 7/18/17.
+//  Copyright © 2017 Bojan Dimovski. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Display {
+	var size: Float
+	var resolution: CGSize
+	var physicalResolution: CGSize
+	var renderedResolution: CGSize
+	var scale: Float
+	var density: Int
+}
