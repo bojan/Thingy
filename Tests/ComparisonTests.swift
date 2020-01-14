5f31ffbb324916abcae507d54692d913f31897ac
Bojan Dimovski <hello@bojandimovski.com>
2020-01-14 21:06:46 +0100

    Rename the unit test folder.

--- Tests/ComparisonTests.swift
+++ Tests/ComparisonTests.swift
@@ -5,7 +5,21 @@
 //  Created by Bojan Dimovski on 07.4.17.
 //  Copyright (c) 2017 Bojan Dimovski. All rights reserved.
 //
-
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//                    Version 2, December 2004
+//
+// Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+//
+// Everyone is permitted to copy and distribute verbatim or modified
+// copies of this license document, and changing it is allowed as long
+// as the name is changed.
+//
+//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
+//
+//
 
 import XCTest
 @testable import Thingy
